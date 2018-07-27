eac9ca789811a4e40cd03cd4fa63119fc0b9af5a
rahimkhan007 <41725651+rahimkhan007@users.noreply.github.com>
2018-07-27 11:06:38 +0530

    Create rahim.java

---
+++ rahim.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int num=3;
+		if(num>0)
+		{
+			System.out.println("num is positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("num is negative");
+			
+		}
+		else
+		{
+			System.out.println("num is zero");
+		}
+	}
+}
